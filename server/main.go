505e4ae1414404f66f9f4ede28ae9eabd1e34f1b
JG Rivera <rivera.jga10@gmail.com>
2022-12-04 18:50:51 +0800

    feat: invoke load database in main

--- server/main.go
+++ server/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/jg-rivera/go-shows/config"
 )
 
 func main() {
 	config.LoadEnv()
-
-	log.Printf("Database: %v", os.Getenv("DATABASE"))
+	config.LoadDatabase()
 
 	http.ListenAndServe("localhost:80", nil)
 }
