227a0147822857c1f7e5f6ff5d64d6eca1c3b5dc
JG Rivera <rivera.jga10@gmail.com>
2022-12-05 21:23:23 +0800

    feat: setup app in main

--- server/main.go
+++ server/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jg-rivera/go-shows/config"
+	"github.com/jg-rivera/go-shows/tickets"
 )
 
 func main() {
 	config.LoadEnv()
 	config.LoadDatabase()
 
-	http.ListenAndServe("localhost:80", nil)
+	db := config.Database
+
+	db.AutoMigrate(&tickets.Ticket{})
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	tickets.RegisterRoutes(router)
+
+	http.ListenAndServe("localhost:80", router)
 }
