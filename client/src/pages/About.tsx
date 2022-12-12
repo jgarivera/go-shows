860ed430808895768deef21d8942143b2fe89a3b
JG Rivera <rivera.jga10@gmail.com>
2022-12-12 22:52:20 +0800

    feat: update about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function About(): JSX.Element {
   return (
-    <React.Fragment>
-      <p>About page</p>
-    </React.Fragment>
+    <div className="m-3">
+      <h1 className="text-3xl font-bold my-3">About</h1>
+      <p>
+        Go shows is a proof-of-concept for an entertainment website that sells
+        shows.
+      </p>
+    </div>
   );
 }
