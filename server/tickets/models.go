db486636a0bf9b80b7afe0874a64d102e46a32bb
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-01-03 21:25:44 +0800

    feat: add ticket model validation

--- server/tickets/models.go
+++ server/tickets/models.go
@@ -1,6 +1,9 @@
 package tickets
 
-import "time"
+import (
+	"net/url"
+	"time"
+)
 
 type Ticket struct {
 	ID          uint      `json:"id" gorm:"primarykey"`
@@ -9,4 +12,22 @@ type Ticket struct {
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+func (t *Ticket) validate() url.Values {
+	errs := url.Values{}
+
+	if t.Name == "" {
+		errs.Add("name", "The name field is required")
+	}
+
+	if t.Price == 0 {
+		errs.Add("price", "The price field is required")
+	}
+
+	if t.Description == "" {
+		errs.Add("description", "The description field is required")
+	}
+
+	return errs
 }
