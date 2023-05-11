5d9f577f0775ae7842eb3f911832cd7f4113e975
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-11 21:31:19 +0800

    fix(client): improve show list subtitle

--- client/src/features/shows/components/ShowList.tsx
+++ client/src/features/shows/components/ShowList.tsx
@@ -10,7 +10,7 @@ export default function ShowList({
   return (
     <div className="m-3">
       <h1 className="text-3xl font-bold my-3">Shows</h1>
-      <p className="mb-3">These shows are now showing</p>
+      <p className="mb-3">Buy tickets for these upcoming shows!</p>
       {shows.map((show) => {
         return <Show key={show.id} show={show} />;
       })}
