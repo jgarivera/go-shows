6dc6eac8cc0d4cd59baede95ebf57f8400227260
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-16 19:09:19 +0800

    feat(client): add show details route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Shows from "./pages/Shows";
 import About from "./pages/About";
+import ShowDetails from "./pages/ShowDetails";
 
 export default function App(): JSX.Element {
   return (
@@ -11,6 +12,7 @@ export default function App(): JSX.Element {
       <Routes>
         <Route path="/" element={<Shows />} />
         <Route path="/about" element={<About />} />
+        <Route path="/shows/:showId" element={<ShowDetails />} />
       </Routes>
     </React.Fragment>
   );
