0ee02106c7b0fc4c0d8fda02e8c83fa5ba7116b4
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-17 21:37:59 +0800

    feat(client): add example shows in show page

--- client/src/pages/Shows.tsx
+++ client/src/pages/Shows.tsx
@@ -3,7 +3,20 @@ import ShowList from "../features/shows/components/ShowList";
 import { Show } from "../features/shows/types";
 
 export default function Shows(): JSX.Element {
-  const [shows] = useState<Show[]>([]);
+  const [shows] = useState<Show[]>([
+    {
+      id: 1,
+      name: "Gopher Jam!",
+      imageUrl: "https://picsum.photos/200/300",
+      description: "Gopher Jam is a must-see concert coming this holiday.",
+    },
+    {
+      id: 2,
+      name: "Song of the Goes",
+      imageUrl: "https://picsum.photos/200/300",
+      description: "A musical.",
+    },
+  ]);
 
   return <ShowList shows={shows} />;
 }
