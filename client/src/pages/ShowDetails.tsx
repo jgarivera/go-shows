7cae65c1cc4b3bf690d7ace107e5191ac5f89c73
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-17 21:39:13 +0800

    feat(client): add sections in show details page

--- client/src/pages/ShowDetails.tsx
+++ client/src/pages/ShowDetails.tsx
@@ -1,12 +1,31 @@
 import { useParams } from "react-router-dom";
+import SectionList from "../features/shows/components/SectionList";
+import { Section } from "../features/shows/types";
 
 export default function ShowDetails(): JSX.Element {
   const { showId } = useParams();
 
+  const sections: Section[] = [
+    {
+      id: 1,
+      name: "Front-row",
+      availableSeats: 10,
+    },
+    {
+      id: 2,
+      name: "Upper box",
+      availableSeats: 10,
+    },
+  ];
+
   return (
     <div>
       <div className="m-3">
-        <h1 className="text-3xl font-bold my-3">Show details of show id: {showId}</h1>
+        <h1 className="text-3xl font-bold my-3">Show {showId}</h1>
+
+        <p className="text-base">Show description</p>
+
+        <SectionList sections={sections} />
       </div>
     </div>
   );
