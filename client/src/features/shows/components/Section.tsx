bc122b79582a800f462244d004f75992b1c9643b
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-18 21:40:53 +0800

    feat(client): use card layout in section component

--- client/src/features/shows/components/Section.tsx
+++ client/src/features/shows/components/Section.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Section as SectionType } from "../types";
 
 interface SectionProps {
@@ -6,8 +7,17 @@ interface SectionProps {
 
 export default function Section({ section }: SectionProps): JSX.Element {
   return (
-    <div className="flex flex-col justify-between p-4 leading-normal">
-      {section.name}
+    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
+      <p className="text-xl">{section.name} (PHP {section.price.toFixed(2)})</p>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        Available seats: {section.availableSeats}
+      </p>
+      <Link
+        to=""
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Select Seats
+      </Link>
     </div>
   );
 }
