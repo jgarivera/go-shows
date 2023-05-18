c2bcf513ec83c9a8548cf70977c09f1121bf5c35
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-18 21:52:33 +0800

    feat(client): conditionally render select seats

--- client/src/features/shows/components/Section.tsx
+++ client/src/features/shows/components/Section.tsx
@@ -6,18 +6,25 @@ interface SectionProps {
 }
 
 export default function Section({ section }: SectionProps): JSX.Element {
+  const hasAvailableSeats = section.availableSeats > 0;
+
   return (
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
-      <p className="text-xl">{section.name} (PHP {section.price.toFixed(2)})</p>
+      <p className="text-xl">
+        {section.name} (PHP {section.price.toFixed(2)})
+      </p>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
         Available seats: {section.availableSeats}
       </p>
-      <Link
-        to=""
-        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Select Seats
-      </Link>
+
+      {hasAvailableSeats && (
+        <Link
+          to=""
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white focus:ring-4 focus:outline-none rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Select Seats
+        </Link>
+      )}
     </div>
   );
 }
