4761c184fd4daf24457d2c8a36be998732774300
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-23 22:50:41 +0800

    feat(client): add seat component

---
+++ client/src/features/shows/components/Seat.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+interface SeatProps {
+  isOccupied: boolean;
+}
+
+export default function Seat({ isOccupied }: SeatProps): JSX.Element {
+  const [isSelected, setSelected] = useState<boolean>(isOccupied);
+
+  function getColor(): string {
+    if (isOccupied) {
+      return "border-slate-900 bg-red-400";
+    }
+
+    return isSelected
+      ? "border-slate-900 bg-blue-400"
+      : "border-slate-900 hover:bg-slate-400";
+  }
+
+  function onClickSeat(): void {
+    if (isOccupied) {
+      return;
+    }
+
+    setSelected(!isSelected);
+  }
+
+  return (
+    <div
+      className={`w-4 h-4 border-2 m-0.5 ${getColor()}`}
+      onClick={onClickSeat}
+    ></div>
+  );
+}
