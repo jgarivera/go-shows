57ca63285354c5fda61a5c0fabb9728691653eeb
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-25 19:48:34 +0800

    feat(client): use dialog in section component

--- client/src/features/shows/components/Section.tsx
+++ client/src/features/shows/components/Section.tsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
-import { Section as SectionType } from "../types";
+import { useState } from "react";
+import { Seat, Section as SectionType } from "../types";
+import SeatSelectionDialog from "./SeatSelectionDialog";
 
 interface SectionProps {
   section: SectionType;
@@ -7,24 +8,49 @@ interface SectionProps {
 
 export default function Section({ section }: SectionProps): JSX.Element {
   const hasAvailableSeats = section.availableSeats > 0;
+  const seats: Seat[] = [
+    { id: 0, isOccupied: true },
+    { id: 1, isOccupied: false },
+  ];
+
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+  const handleDialogOpen = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
+
+  const handleDialogCheckout = () => {};
 
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
-      <p className="text-xl">
-        {section.name} (PHP {section.price.toFixed(2)})
-      </p>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        Available seats: {section.availableSeats}
-      </p>
+    <>
+      <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
+        <p className="text-xl">
+          {section.name} (PHP {section.price.toFixed(2)})
+        </p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Available seats: {section.availableSeats}
+        </p>
 
-      {hasAvailableSeats && (
-        <Link
-          to=""
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white focus:ring-4 focus:outline-none rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Select Seats
-        </Link>
-      )}
-    </div>
+        {hasAvailableSeats && (
+          <button
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white focus:ring-4 focus:outline-none rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={handleDialogOpen}
+          >
+            Select Seats
+          </button>
+        )}
+      </div>
+
+      <SeatSelectionDialog
+        isOpen={isDialogOpen}
+        onCheckout={handleDialogCheckout}
+        onClose={handleDialogClose}
+        seats={seats}
+      />
+    </>
   );
 }
